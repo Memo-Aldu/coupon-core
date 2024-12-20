d8cadc51afd036308d582f560d73ea9007ad8166
Memo- Aldu <71087733+Memo-Aldu@users.noreply.github.com>
2024-12-20 17:52:40 -0500

    Moved validation logic to validation.go

--- coupon/model.go
+++ coupon/model.go
@@ -1,11 +1,9 @@
 package coupon
 
 import (
-	"errors"
 	"log"
 	"time"
 	"encoding/json"
-	"strings"
 )
 
 type DiscountType string
@@ -32,32 +30,6 @@ type CreateCouponRequest struct {
 	ApplicableProducts  json.RawMessage `json:"applicable_products"`
 	IsActive 		 	bool      		`json:"is_active"`
 	UserSpecific 		bool      		`json:"user_specific"`
-}
-
-
-func (req *CreateCouponRequest) Validate() error {
-	var errors ValidationErrors
-	if req.Code == "" {
-		errors = append(errors, NewValidationError("code", "code is required"))
-	}
-	if !DiscountType(req.DiscountType).IsValid() {
-		errors = append(errors, NewValidationError("discount_type", "invalid discount type"))
-	}
-	if req.Value <= 0 {
-		errors = append(errors, NewValidationError("value", "value must be greater than zero"))
-	}
-	if req.MaxRedemptions < 0 {
-		errors = append(errors, NewValidationError("max_redemptions", "max_redemptions cannot be negative"))
-	}
-	if req.MinimumOrderValue < 0 {
-		errors = append(errors, NewValidationError("minimum_order_value", "minimum_order_value cannot be negative"))
-	}
-
-	if len(errors) > 0 {
-        return errors
-    }
-
-	return nil
 }
 
 
@@ -102,30 +74,8 @@ type CouponRedemption struct {
 }
 
 
-func (r *CouponRedemption) Validate() error {
-	var errors ValidationErrors 
-	if r.CouponId <= 0 {
-		errors = append(errors, NewValidationError("coupon_id", "coupon_id must be greater than zero"))
-	}
-
-	if r.CouponUserId <= 0 {
-		errors = append(errors, NewValidationError("coupon_user_id", "coupon_user_id must be greater than zero"))
-	}
-
-	if r.OrderId <= 0 {
-		errors = append(errors, NewValidationError("order_id", "order_id must be greater than zero"))
-	}
-
-	if len(errors) > 0 {
-		return errors
-	}
-	
-	return nil
-}
-
-
-func NewCoupon(code string, discountType DiscountType, value, minimumOrderValue float64, maxRedemptions int, expiryDate string, 
-	applicableProducts []string, IsActive, isUserSpecific bool ) *Coupon {
+func NewCoupon(code string, discountType DiscountType, value, minimumOrderValue float64, 
+	maxRedemptions int, expiryDate string, applicableProducts []string, IsActive, isUserSpecific bool ) *Coupon {
 	
 	date, err := parseDate(expiryDate)
 	if err != nil {
@@ -145,38 +95,4 @@ func NewCoupon(code string, discountType DiscountType, value, minimumOrderValue 
 		UserSpecific: isUserSpecific,
 		CreatedAt: time.Now().UTC(),
 	}
-}
-
-
-func parseDate(date string) (time.Time, error) {
-	parsed, err := time.Parse("2006-01-02T15:04:05Z", date)
-	if err != nil {
-		return time.Time{}, errors.New("invalid date format, expected '2006-01-02T15:04:05Z'")
-	}
-	return parsed, nil
-}
-
-
-type ValidationError struct {
-	Field   string
-	Message string
-}
-
-func (e *ValidationError) Error() string {
-	return e.Field + ": " + e.Message
-}
-
-func NewValidationError(field, message string) error {
-	return &ValidationError{Field: field, Message: message}
-}
-
-
-type ValidationErrors []error
-
-func (v ValidationErrors) Error() string {
-    var messages []string
-    for _, err := range v {
-        messages = append(messages, err.Error())
-    }
-    return strings.Join(messages, ", ")
 }
--- coupon/validation.go
+++ coupon/validation.go
@@ -2,6 +2,9 @@ package coupon
 
 import (
 	"fmt"
+	"strings"
+	"time"
+	"errors"
 )
 
 
@@ -23,4 +26,87 @@ func ValidateCreateCouponRequest(coupon *Coupon) error {
 	}
 
 	return nil
+}
+
+
+func (r *CouponRedemption) Validate() error {
+	var errors ValidationErrors 
+	if r.CouponId <= 0 {
+		errors = append(errors, NewValidationError("coupon_id", "coupon_id must be greater than zero"))
+	}
+
+	if r.CouponUserId <= 0 {
+		errors = append(errors, NewValidationError("coupon_user_id", "coupon_user_id must be greater than zero"))
+	}
+
+	if r.OrderId <= 0 {
+		errors = append(errors, NewValidationError("order_id", "order_id must be greater than zero"))
+	}
+
+	if len(errors) > 0 {
+		return errors
+	}
+	
+	return nil
+}
+
+
+func (req *CreateCouponRequest) Validate() error {
+	var errors ValidationErrors
+	if req.Code == "" {
+		errors = append(errors, NewValidationError("code", "code is required"))
+	}
+	if !DiscountType(req.DiscountType).IsValid() {
+		errors = append(errors, NewValidationError("discount_type", "invalid discount type"))
+	}
+	if req.Value <= 0 {
+		errors = append(errors, NewValidationError("value", "value must be greater than zero"))
+	}
+	if req.MaxRedemptions < 0 {
+		errors = append(errors, NewValidationError("max_redemptions", "max_redemptions cannot be negative"))
+	}
+	if req.MinimumOrderValue < 0 {
+		errors = append(errors, NewValidationError("minimum_order_value", "minimum_order_value cannot be negative"))
+	}
+
+	if len(errors) > 0 {
+        return errors
+    }
+
+	return nil
+}
+
+
+func parseDate(date string) (time.Time, error) {
+	parsed, err := time.Parse("2006-01-02T15:04:05Z", date)
+	if err != nil {
+		return time.Time{}, errors.New("invalid date format, expected '2006-01-02T15:04:05Z'")
+	}
+	return parsed, nil
+}
+
+
+type ValidationError struct {
+	Field   string
+	Message string
+}
+
+func (e *ValidationError) Error() string {
+	return e.Field + ": " + e.Message
+}
+
+func NewValidationError(field, message string) error {
+	return &ValidationError{Field: field, Message: message}
+}
+
+
+type ValidationErrors []error
+
+
+func (v ValidationErrors) Error() string {
+    var messages []string
+    for _, err := range v {
+        messages = append(messages, err.Error())
+    }
+    return strings.Join(messages, ", ")
 }
